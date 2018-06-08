fb752c2a15fd7fd14985f7c5c958154ea02a9418
Eric Matysek <eric.matysek@gmail.com>
2018-06-08 00:42:59 -0400

    Created models.py and added models for summoners and user_matches tables. Updated TODO.

---
+++ models.py
@@ -0,0 +1,56 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+def db_connect():
+    # TODO move db to config file
+    return create_engine('sqlite:///riot.db', echo=True)
+
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+
+
+class Summoner(Base):
+    __tablename__ = 'summoners'
+
+    summonerid = Column(Integer, primary_key=True, nullable=False)
+    accountid = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String)
+    profileiconid = Column(Integer)
+    revisiondate = Column(Integer)
+    summonerlevel = Column(Integer)
+
+    matches = relationship("UserMatch", back_populates="summoner")
+
+    def __repr__(self):
+        return "<Summoner(summonerid={}, accountid={}, name={}, profileiconid={}, revisiondate={}, summonerlevel={})>" \
+            .format(self.summonerid, self.accountid, self.name, self.profileiconid, self.revisiondate,
+                    self.summonerlevel)
+
+
+class UserMatch(Base):
+    __tablename__ = 'user_matches'
+
+    id = Column(Integer, primary_key=True)
+    accountid = Column(Integer, ForeignKey('summoners.accountid'))
+    lane = Column(String)
+    gameid = Column(Integer)
+    champion = Column(Integer)
+    platformid = Column(String)
+    timestamp = Column(Integer)
+    queue = Column(Integer)
+    role = Column(String)
+    season = Column(Integer)
+
+    summoner = relationship("Summoner", back_populates="matches")
+
+    def __repr__(self):
+        return "<UserMatch(accountid={}, lane={}, gameid={}, champion={}, platformid={}, timestamp={}, queue={}," \
+               "role={}, season={})>".format(self.accountid, self.lane, self.gameid, self.champion, self.platformid,
+                                             self.timestamp, self.queue, self.role, self.season)
