5664434f2dd5e8a42de2664a06b86423a0acab4f
Eric Matysek <eric.matysek@gmail.com>
2018-06-18 21:52:55 -0400

    Updated webui config

--- ui/webui.py
+++ ui/webui.py
@@ -1,20 +1,12 @@
 from flask import Flask, render_template
 from util.request_handler import RequestHandler
-import json
 
 app = Flask(__name__)
-
-configFile = '../conf/config.json'
-configData = json.load(open(configFile))
-
-apiEndpoint = configData["api"]["endpoint"]
-apiKey = configData["api"]["key"]
-#rh = RequestHandler(apiEndpoint, apiKey, '../riot.db')
 
 
 @app.route('/')
 def home():
-    rh = RequestHandler(apiEndpoint, apiKey, '../riot.db')
+    rh = RequestHandler()
     sums = rh.get_all_summoners()
     um = rh.get_all_usermatches()
     rh.close()
