ffac69f3174c07b3bf833adf7e23e51e1790c485
Eric Matysek <eric.matysek@gmail.com>
2018-08-30 02:41:53 -0400

    Change default sqlite db location to project root folder. Delete config.json.example in favor of env configs.

--- app/default_conf.py
+++ app/default_conf.py
@@ -20,5 +20,5 @@ def init_env():
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '9867eea89c294b9162dba0890b7dfb98'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
